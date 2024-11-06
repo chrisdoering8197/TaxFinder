5a81716b847bd96a16589199bacb8c04f4a5f0e9
cdoering8197 <cdoering@mit.edu>
2024-11-06 10:23:32 -0500

    made changes to improve runtime

--- scripts/find_systems_in_FT.py
+++ scripts/find_systems_in_FT.py
@@ -1,9 +1,11 @@
 import glob
 import os
+import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
 from Bio import SearchIO
 from Bio import SeqIO
+from tqdm import tqdm
 from snakemake.script import snakemake
 
 phmmer_file = snakemake.input[0]
@@ -20,27 +22,21 @@ FT_files = glob.glob(os.path.join(FT_folder_path,'*_feature_table.txt'))
 assemblySums = pd.read_csv(taxa_file_path,sep = '\t',skiprows = 1,usecols = ['#assembly_accession','species_taxid'])
 Acc2Taxid = assemblySums.set_index('#assembly_accession')['species_taxid'].to_dict()  
 
-phmmer_hits = {}
-for record in SearchIO.parse(phmmer_file,'hmmer3-tab'):
-    hits = {x.id for x in record.hits}
-    phmmer_hits[record.id] = hits
+print('Searching feature tables for system matches...')
+
+phmmer_hits = {record.id:{x.id for x in record.hits} for record in SearchIO.parse(phmmer_file,'hmmer3-tab')}
 
 def checkFT(FT_path):
     
-    FT = pd.read_csv(FT_path,sep = '\t')
+    FT = pd.read_csv(FT_path,sep = '\t',usecols=['# feature','non-redundant_refseq','assembly'])
     FT = FT.loc[FT['# feature'] == 'CDS'].reset_index(drop=True)
     all_prots = set(FT['non-redundant_refseq'])
+     
+    indices_in_ft = {protein_id: FT[FT['non-redundant_refseq'].isin(all_prots.intersection(hits))].index.to_list()
+                     for protein_id, hits in phmmer_hits.items() if all_prots.intersection(hits)}
     
-
-    indices_in_ft = {}
-    for protein_id in phmmer_hits:
-        overlapping_hits = all_prots.intersection(phmmer_hits[protein_id])
-        if len(overlapping_hits) == 0: #If any of the proteins are not present return
-            return []
-        indices_in_ft[protein_id] = FT[FT['non-redundant_refseq'].isin(overlapping_hits)].index
-    indices_in_ft = {}
-    
- 
+    if not all([True if indices else False for ID, indices in indices_in_ft.items()]):
+        return []
     
     Acc = FT.at[0,'assembly']
     ID = Acc2Taxid[Acc]
@@ -50,23 +46,22 @@ def checkFT(FT_path):
         return [(Acc,ID)]
     
     search_dist = len(prot_names) + buff
-    near_another = {k:False for k,v in indices_in_ft.items()}
-    for protein_i in indices_in_ft:
-        i_indices = indices_in_ft[protein_i] 
-        for protein_j in indices_in_ft:
+    near_another = {protein_id: False for protein_id in indices_in_ft}
+    for protein_i, i_indices in indices_in_ft.items():
+        i_array = np.array(i_indices)
+        for protein_j, j_indices in indices_in_ft.items():
             if protein_i != protein_j:
-                j_indices = indices_in_ft[protein_j]
-                for index_i in i_indices:
-                    for index_j in j_indices:
-                        if abs(index_i-index_j) <= search_dist:
-                            near_another[protein_i] = True
-                            near_another[protein_j] = True
-    if all(near_another.values()):
-        return [(Acc,ID)]
-    else:
-        return []
-    
-results=Parallel(n_jobs=threads)(delayed(checkFT)(x) for x in FT_files)
-results = pd.DataFrame(results)
+                j_array = np.array(j_indices)
+                dist_matrix = np.abs(i_array[:,None] - j_array)
+                if (dist_matrix <= search_dist).any():
+                    near_another[protein_i] = True
+                    near_another[protein_j] = True
+
+    return [(Acc,ID)] if all(near_another.values()) else []
+
+results=Parallel(n_jobs=threads)(delayed(checkFT)(x) for x in tqdm(FT_files))
+results = pd.DataFrame(results,columns=['Acc_TaxID'])
+results[['Accession','TaxID']] = pd.DataFrame(results['Acc_TaxID'].tolist(),index=results.index)
+results.drop(columns=['Acc_TaxID'],inplace=True)
 results.dropna(inplace=True)
 results.to_csv(out,sep='\t',index=False,header=False)
